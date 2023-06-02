2d4532b58e76a2628913e1bda4eb92cad28934fe
MatheusAndradeeee <mateusandracosta@gmail.com>
2023-06-02 13:29:35 -0400

    segundo

---
+++ atv3Aumento/Empregado.cs
@@ -0,0 +1,67 @@
+﻿
+
+/*3. Crie uma classe Empregado que possui os seguintes atributos: nome, salário, data de
+admissão. Todos os atributos devem ser privados e possuírem os métodos sets e gets.
+a) Construa o método construtor para obrigar a inserir todos os atributos do objeto.
+b) Construa um método que realize o aumento de salário do funcionário, esse método deve
+receber por parâmetro a porcentagem do aumento.
+c) Construa um atributo estático na classe Empregado que deve armazenar o valor das
+somas dos salários dos funcionários. Caso algum funcionário receber aumento, o valor
+desse atributo deve ser atualizado (esse atributo é necessário para verificar quantos de
+funcionário a empresa está pagando por mês).*/
+using System.Security.Cryptography.X509Certificates;
+
+public class Empregado
+{
+    private string nome = "";
+    private double salario;
+    private DateTime data;
+    public static double somas = 0;
+
+    public Empregado(string nome, double salario, DateTime data)
+    {
+        this.nome = nome;
+        this.salario = salario;
+        this.data = data;
+        somas += salario;
+    }
+
+
+
+   public void Aumento(double x) 
+   {
+        double re = (x / 100)*salario;
+        salario += re;
+        somas += re; 
+    }
+
+    public string GetNome()
+    {
+        return this.nome;
+    }
+    public void SetNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+
+        public double GetSalario()
+        {  
+        return this.salario;
+        }
+
+    public void SetSalario(double salario)
+    {
+        this.salario = salario;
+    }
+
+    public DateTime GetData()
+    {
+       return this.data;  
+    }
+    public void SetData(DateTime data)
+    {
+        this.data = data;
+    }
+}
+
---
+++ atv3Aumento/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+
+        while (true)
+        {
+            Console.Clear();
+            Console.Write("Dígite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Dígite seu salario: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Dígite sua data de admissão: ");
+            DateTime data = Convert.ToDateTime(Console.ReadLine());
+
+
+            Empregado a = new Empregado(nome, salario, data);
+
+            Console.WriteLine("\nIrá receber aumento? \n1 - sim \n2 - não");
+
+            int sim = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (sim)
+            {
+
+                case 1:
+                    Console.Write("Dígite o valor do aumento: ");
+                    double aumento = Convert.ToDouble(Console.ReadLine());
+                    a.Aumento(aumento);
+                    Console.WriteLine($"\n{nome}, você começou na data {data}, seu salario é de {salario.ToString("c2")} e com aumento fica {a.GetSalario().ToString("c2")}");
+                    break;
+                case 2:
+                    Console.WriteLine($"\n{nome}, você começou na data {data}, seu salario é de {salario.ToString("c2")}");
+                    break;
+
+            }
+
+            Console.WriteLine("\nSoma total:" + Empregado.somas.ToString("c2")+"\n");
+            Console.WriteLine("Adicionar outro funcionario na folha de pagamento?");
+            Console.WriteLine("1 - sim\n2 - não");
+
+            int digi= Convert.ToInt32(Console.ReadLine());
+
+            if (digi == 2)
+            {
+                break;
+            }
+        }
+        
+    }
+}
